0389604aac35a7a895f07a75f7e9306cee9844fc
RDybkjaer <59032013+RDybkjaer@users.noreply.github.com>
2021-09-14 13:43:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,37 +40,39 @@ black -45 -35
 
 
 def loop: #MAIN CODE - THIS CODE CALLS THE SUBFUNCTIONS
-    while = TRUE:
+    for sekvens in range(1,10)
         autoDrive(DIST=100)
 
         if sekvens == 1:
             sekvens1()
-            sekvens = sekvens + 1
 
         elif sekvens == 2:
-            sekvens2()
-            sekvens = sekvens + 1
-            
+            sekvens2()            
 
         elif sekvens == 3:
-            sekvens3()
-            sekvens = sekvens + 1
+            sekvens3()           
 
         elif sekvens == 4:
-            sekvens4()
-            sekvens = sekvens + 1
+            sekvens4()            
 
         elif sekvens == 5:
-            break
+            sekvens5()
+
+        elif sekvens == 6:
+            sekvens6()
 
         elif sekvens == 7:
-            break
+            sekvens7()
 
         elif sekvens == 8:
-            break
+            sekvens6()
 
-        else sekvens == 9:
-            break
+        elif sekvens == 9:
+            sekvens9()
+        
+        else:
+            ev3.speaker.play_file(SoundFile.FANFARE)
+
 
 def sekvens1(): #CHANGE LANE TWO DIRECTIONS - READY FOR TEST
     '''
@@ -79,9 +81,6 @@ def sekvens1(): #CHANGE LANE TWO DIRECTIONS - READY FOR TEST
     turn_drive_ignore(0, 'RIGHT')
     autoDrive(DIST=100)
     turn_drive_ignore(0, 'LEFT')
-    sekvens = sekvens +1
-
-    break
 
 def sekvens2(): #PICK UP WATERBOTTLE - READY FOR TEST
     '''
@@ -110,7 +109,6 @@ def sekvens2(): #PICK UP WATERBOTTLE - READY FOR TEST
     while deviation >= 35:
         robot.drive(100)
     robot.stop()
-    break
 
 def sekvens3(): #SEESAW - UNFINISHED
     '''
@@ -125,10 +123,49 @@ def sekvens3(): #SEESAW - UNFINISHED
 def sekvens4(): #CHANGE LANE TWICE - READY FOR TEST
     autoDrive(DIST=10)
     turn_drive_ignore(1, 'LEFT')
-    
-    break
+
+def sekvens5(): #UNFINISHED
+    '''
+    Ingen idé so far.
+    Drej til venstre
+    Fortsæt frem til stregen
+    Kør ind på midten vha robot.straight.
+    Drej til du kan se flasken
+    Saml flasken op - bør man lave en fællesfunktion for sekvens 2 og 5?
+    Bak ud og tæl stregerne - turn_drive_ignore??
+    Stil flasken
+    Bak væk.
+    Find ud.
+    Drej til venstre ved udgangen
+    '''
+
+def sekvens6(): #USED BOTH FOR SEKVENS6 & 8 - UNFINISHED
+    '''
+    NOTHING TO SEE HERE
+    '''
+
+def sekvens7(): #UNFINISHED
+    '''
+    NOTHING TO SEE HERE
+    '''
+
+def sekvens9(): #UNFINISHED
+    '''
+    NOTHING TO SEE HERE
+    '''
+
+
+
+
 
 def turn_drive_ignore(NUMBER_TO_IGNORE, SIDE_TO_TURN) #BASIC CODE FOR CHANGING LANE - USED IN SEKVENS 1 & 4
+    '''
+    Turn_drive_ignore is used to change lanes, by ignoring other lanes.
+    Defined by SIDE_TO_TURN, it'll turn either 45 degrees right, left or not at all.
+
+    An idea might be to add at "forwards or backwards" argument, so it can function for sequence 5 as well.
+
+    '''
     deviation = deviationCheck()
     
     #Following lines defines which way to turn. If no side is given, it'll beep and stop the function
@@ -137,123 +174,7 @@ def turn_drive_ignore(NUMBER_TO_IGNORE, SIDE_TO_TURN) #BASIC CODE FOR CHANGING L
     elif SIDE_TO_TURN == 'LEFT':
         turnAngle = -45
     else:
-        for i in range(5):
-            ev3.speaker.beep()
-            wait(100)
-        break
-    
-    robot.turn(turnAngle):
-    robot.drive(360): #drive with 360deg/sec
-
-    while i < NUMBER_TO_IGNORE:
-        if deviaition >= deviationcheck
-            i = i+1
-        
-        deviation = deviaitionCheck:
-    robot.stop()
-
-    robot.turn(turnAngle * -1)
-    break
-
-def straightenUp (): #BASIC CODE TO KEEP ON THE LINE - USED IN AUTODRIVE
-    '''
-    straightenUp helps the robot return to the line.
-    It turns 50 degrees in each direction and checks wether the line shows up. C destines wether it turns right or left.
-    Returns the angle at which it found the line, to be used in later corrections
-    '''
-    deviation = deviationCheck()
-    while deviation >= 30: #white
-        c = 1
-        for i in range(0,50*c,10*c):
-            robot.turn(i):
-            deviation = deviationCheck()
-        robot.turn(-50)
-        c = c * -1
-    return i
-
-def autoDrive(BREAKABLE=0, DIST=100): #BASECODE TO DRIVE WHILE ON THE LINE
-    '''
-    autoDrive is a function that drives the robot while looking for deviation to the line.
-    It is supposed to have a distance driven added
-    '''
-    while deviaition <= -35: #black
-        if deviation < 30:
-            robot.straight(DISTANCE)
-            deviation = deviationCheck()
-        else:
-            '''
-            straightenUp guides the robot back on track, and returns the turning value it needed to do(correction).
-            This allows us to center the robot on the line(5 cm to center?) and return to driving
-            '''
-            correction = straightenUp()
-            robot.straight(50):
-            robot.turn(correction)
-            deviation = deviationCheck()
-        
-        if BREAKABLE == TRUE #Tells us wether to stop at a black line, or only drive the distance given in "distance"
-            break
-    
-def deviationCheck (): # CHECKING DEVIATION. To be used in deviation = deviationCheck()
-    break
-
-def sekvens2(): #PICK UP WATERBOTTLE - READY FOR TEST
-    '''
-    The sequence used by the robot to finish task 3, the waterbottle task
-    '''
-
-    #Following lines drives towards the bottle and picks it up
-    sensorVar = ultra_sensor.distance()
-    robot.turn(45)
-    while sensorVar > 50:
-        autoDrive(BREAKABLE=1,DIST=50)
-        sensorVar = ultra_sensor.distance()
-    claw_motor.run_time(360, 5000)
-    
-    #Following lines drives towards the black line
-    deviation = deviationCheck()
-    while deviation >= -35:
-        robot.drive(100)
-    robot.stop()
-
-    #Folloing lines puts down the bottle and return back to the track
-    claw_motor.runtime(-360, 5000)
-    robot.straigt(100)
-    robot.turn(-145)
-
-    while deviation >= 35:
-        robot.drive(100)
-    robot.stop()
-    break
-
-def sekvens3(): #SEESAW - UNFINISHED
-    '''
-    Function to do task 3, the seesaw.
-    An idea might be to put on a gyroscope and make to robot stop when the movement happens?
-    '''
-    robot.turn(45)
-    robot.straight(100)
-    autoDrive(DIST=100)
-    autoDrive(DIST=10)
-
-def sekvens4(): #CHANGE LANE TWICE - READY FOR TEST
-    autoDrive(DIST=10)
-    turn_drive_ignore(1, 'LEFT')
-    
-    break
-
-def turn_drive_ignore(NUMBER_TO_IGNORE, SIDE_TO_TURN) #BASIC CODE FOR CHANGING LANE - USED IN SEKVENS 1 & 4
-    deviation = deviationCheck()
-    
-    #Following lines defines which way to turn. If no side is given, it'll beep and stop the function
-    if SIDE_TO_TURN == 'RIGHT':
-        turnAngle = 45
-    elif SIDE_TO_TURN == 'LEFT':
-        turnAngle = -45
-    else:
-        for i in range(5):
-            ev3.speaker.beep()
-            wait(100)
-        break
+        turnAngle = 0
     
     robot.turn(turnAngle):
     robot.drive(360): #drive with 360deg/sec
